dd59cb1d574b516cacf79d96c4a757d180adf200
Jayakrishnan <jayakrishnan800@gmail.com>
2022-09-27 14:32:07 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:4200'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));

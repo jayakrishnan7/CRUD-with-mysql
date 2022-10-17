6439ebb82d9402bd02ab2168f92a5673c2f79263
Jayakrishnan <jayakrishnan800@gmail.com>
2022-10-17 17:31:55 +0530

    mysql connection errorrr....

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import mysql from 'mysql';
+import * as util from './util.json'
 import connects  from './server/config/db';
 import { router } from './server/routes/routes';
 
+// requireing path module...................
 const path = require('path')
 
 // cross origin resource sharing............
@@ -10,12 +13,91 @@ const cors = require('cors');
 // morgan for logs............
 const morgan = require('morgan');
 
+// mysql setup................
+// mysql module imported......
+var pool : any ;
+
+
+pool = mysql.createPool({
+  // host: 'habit.czakalaam8ah.ap-south-1.rds.amazonaws.com',
+  host: 'localhost',
+  // user: 'admin',
+  user: 'root',
+  password: 'root',
+  //password: 'shivam25',
+  database: 'demo',
+  // insecureAuth: true
+});
+
+
+
+declare global { var mysqldbconnection: any };
+
+
+var mysqldbconnection = {
+  query: function () {
+    var sql_args: any = [];
+    var args: any = [];
+    for (var i = 0; i < arguments.length; i++) {
+      args.push(arguments[i]);
+    }
+    var callback = args[args.length - 1]; //last arg is callback
+    pool.getConnection(function (err: any, connection: any) {
+      if (err) {
+        console.log(err);
+        return callback(err);
+      }
+      if (args.length > 2) {
+        sql_args = args[1];
+      }
+      connection.query(args[0], sql_args, function (err: any, results: any) {
+        connection.release(); // always put connection back in pool after last query
+        if (err) {
+          console.log(err);
+          return callback(err);
+        }
+        callback(null, results);
+      });
+    });
+  }
+};
+
+
+
+declare global { var execQuery: any };
+
+var execQuery = (query: any, params: any) => {
+  return new Promise((resolve, reject) => {
+    console.log('dddddddddddd');
+    
+    mysqldbconnection.query(
+      query, params, (error: any, result: any) => {
+        if (error) {
+          console.log(error);
+          reject(util.errorOccured);
+        } else {
+          resolve(result);
+        }
+    }
+    );
+
+  })
+} 
+
+
+
+
+
+
+
+
+
 const app = express();
 
 const PORT = 4001; 
 
 // db connection.............
-connects();
+// connects();
 
 //parse json bodies...........
 app.use(express.urlencoded({ extended: true }));
--- src/server/config/db.ts
+++ src/server/config/db.ts
@@ -14,4 +14,52 @@ function connects () {
 }
 
 
+
+// function connects () {
+    
+//     return  global.mysqldbconnection = {
+//         query: function () {
+//           var sql_args: any = [];
+//           var args: any = [];
+//           for (var i = 0; i < arguments.length; i++) {
+//             args.push(arguments[i]);
+//           }
+//           var callback = args[args.length - 1]; //last arg is callback
+//           pool.getConnection(function (err: any, connection: any) {
+//             if (err) {
+//               console.log(err);
+//               return callback(err);
+//             }
+//             if (args.length > 2) {
+//               sql_args = args[1];
+//             }
+//             connection.query(args[0], sql_args, function (err: any, results: any) {
+//               connection.release(); // always put connection back in pool after last query
+//               if (err) {
+//                 console.log(err);
+//                 return callback(err);
+//               }
+//               callback(null, results);
+//             });
+//           });
+//         }
+//       };
+      
+      
+//     .then(() => {
+//         console.log('DB connected successfully');
+        
+//     })
+//     .catch((error: any) => {
+//         console.log(error);
+        
+//     })
+// }
+
+
 export default connects;
+
+
+
+
+

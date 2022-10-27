3593f798c532a3ec71b562958ea9eaee24a353d8
jayakrishnan-mk <jayakrishnansfc43@gmail.com>
2022-10-27 16:34:59 +0530

    crud with mysql completed and multer api started

--- src/server/controllers/users.ts
+++ src/server/controllers/users.ts
@@ -2,7 +2,32 @@ import { Request, Response } from "express";
 import UserModel from "../models/users";
 
 import mysql from "mysql";
-import execQuery from "../config/db";
+// import execQuery from "../config/db";
+
+//importing crypto module to generate random binary data
+import CryptoJS from "crypto-js";
+
+// var cron = require('node-cron');
+import cron from "node-cron";
+
+// date formatting..........
+import moment from "moment";
+
+// send files to mail.........
+import nodemailer from "nodemailer";
+
+//creating excel file........
+import xlsx from "xlsx";
+
+// creating unique id........
+import { v4 } from "uuid";
+
+// multer for file uploads....
+import multer from "multer";
+
+const excelJS = require("exceljs");
+// import * as excelJS from 'exceljs'
+// import excel from 'exceljs';
 
 // var pool = mysql.createConnection({
 var pool = mysql.createPool({
@@ -11,21 +36,6 @@ var pool = mysql.createPool({
   password: "root",
   database: "crud",
 });
-
-//importing crypto module to generate random binary data
-import CryptoJS from "crypto-js";
-
-// var cron = require('node-cron');
-
-import cron from "node-cron";
-import moment from "moment";
-import nodemailer from "nodemailer";
-import xlsx from "xlsx";
-import { v4 } from "uuid";
-
-const excelJS = require("exceljs");
-// import * as excelJS from 'exceljs'
-// import excel from 'exceljs';
 
 // ....... all users fetching....................
 const allUsers = async (req: Request, res: Response) => {
@@ -87,7 +97,7 @@ const createPerson = async (req: Request, res: Response) => {
       // console.log('rrrrrrrrrrrrrrrrr', result?.length);
 
       if (!result?.length) {
-        console.log("its a chance..........");
+        // console.log("its a chance..........");
 
         const sql = `Insert Into students (
           name, 
@@ -126,8 +136,6 @@ const createPerson = async (req: Request, res: Response) => {
         );
       }
 
-
-
       // ...........................................password.decrypting.....................................................
 
       const decryptUser = userData.password;
@@ -140,9 +148,7 @@ const createPerson = async (req: Request, res: Response) => {
       console.log("decrypted password..........", originalText);
 
       // ------------------------------------------------------------------------------------------------------------------
-
     });
-
   } catch (error) {
     console.log("errrrr", error);
     res.send({ status: 0, error: error });
@@ -152,40 +158,109 @@ const createPerson = async (req: Request, res: Response) => {
 // ....... edit user....................
 const updateUser = async (req: Request, res: Response) => {
   try {
+    const id = req.params.id;
+    // console.log('uuuuuuuuu', id);
+
     // console.log("idsssssssss", req.body);
+
+    // let student = req.body;
+
+    // console.log('stttttttt', student);
+
     const { name, classNumber, email, password, phone, dob, photo, isDeleted } =
       req.body;
 
-    const data = await UserModel.findByIdAndUpdate(req.params.id, req.body);
-    res.json({ message: "User profile updated successfully..." });
+    // console.log('eeeeeeeeeeeeeee', email);
+
+    // const userData = {
+    //   name: req.body.name,
+    //   classNumber: req.body.classNumber,
+    //   email: req.body.email,
+    //   password: req.body.password,
+    //   phone: req.body.phone,
+    //   dob: req.body.dob,
+    //   photo: req.body.photo,
+    //   isDeleted: req.body.isDeleted,
+    // };
+
+
+    // console.log('dddddddd', dob);
+    // console.log('tttttt', typeof(dob));
+    
+    // const dateFormatting = moment.utc(dob, "DD/MM/YYYY").toDate();
+    // const newDate = moment.utc(dateFormatting).format('DD/MM/YYYY');
+    
+    // var formatted = moment(dob).format("DD-MM-YYYY");  
+    var formatted = moment(dob). format('YYYY-MM-DD')
+
+    console.log('newddddddd', formatted);
+    console.log('tyyyyyyy', typeof(formatted));
+    
+
+    const sql = `
+    UPDATE students 
+        SET name = "${name}", 
+        classNumber = "${classNumber}",  
+        email  = "${email}", 
+        password  = "${password}", 
+        phone  = "${phone}", 
+        dob   = "${formatted}", 
+        photo  = "${photo}", 
+        isDeleted = "${isDeleted}"
+
+        WHERE id = "${id}"
+       `;
+
+
+    pool.query(
+      sql,
+      [name, classNumber, email, phone, dob, photo, isDeleted],
+      (err, result, fields) => {
+        if (err) {
+          res.send({ status: 0, data: err });
+        } else {
+          // result.forEach((element: any) => {
+          //   if(element.constructor == Array) {
+          //     res.send('Inserted student id:  ' + element[0].id)
+          //   }
+          // })
+          res.json({
+            message: "Updated successfully...",
+            status: 1,
+            data: result,
+          });
+        }
+      }
+    );
+
+
   } catch (error) {
     console.log(error);
     res.status(500).send("Updation failed!!");
   }
 };
 
-
-// ........permanent delete user...................
-// const deletePerson = async (req: Request, res: Response) => {
-//   try {
-//     await UserModel.findByIdAndDelete(req.params.id);
-//     res.send("User deleted successfully...");
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send(error);
-//   }
-// };
-
-// .......soft delete user...................
+// ....... delete user...................
 const deletePerson = async (req: Request, res: Response) => {
   try {
-    const person: any = await UserModel.updateOne(
-      { _id: req.params.id },
-      { $set: { isDeleted: true } }
-    );
+    const id = req.params.id;
 
-    res.json({
-      message: "User deleted Successfully..",
+    console.log("deeeelllllllll", id);
+
+    const sql = `
+      DELETE FROM students
+      WHERE id = "${id}"`;
+
+    pool.query(sql, [id], (err, result, fields) => {
+      if (err) {
+        res.send({ status: 0, data: err });
+      } else {
+        res.json({
+          message: "Deleted successfully...",
+          status: 1,
+          data: result,
+        });
+      }
     });
   } catch (error) {
     console.log(error);
@@ -196,84 +271,45 @@ const deletePerson = async (req: Request, res: Response) => {
 // ....... search users....................
 const searchUsers = async (req: Request, res: Response) => {
   try {
-    let userData = req.body;
+    // let userData = req.body;
+
+    let classNumber = req.body.classNumber;
+    let searchText = req.body.searchText;
+
+    // console.log('sssssssssssss', classNumber, searchText);
+
     // let skip = userData.skip || 0;
-    let limit = userData.limit || 10;
-    let advanceQuery: any;
-    advanceQuery = await createAdvanceQuery(userData);
-    // console.log("advance query", advanceQuery);
+    // let limit = userData.limit || 10;
 
-    let finalQuery = {};
-    // console.log(advanceQuery.finalFilterQuery);
-    if (advanceQuery.finalFilterQuery) {
-      finalQuery = JSON.parse(`{${advanceQuery.finalFilterQuery}}`);
-    }
-    // console.log(JSON.stringify(finalQuery));
+    let sql =
+      "SELECT * FROM students WHERE name LIKE '%" +
+      searchText +
+      "%' OR email LIKE '%" +
+      searchText +
+      "%' OR phone LIKE '%" +
+      searchText +
+      "%'";
 
-    // console.log('ffffffffff', finalQuery);
-
-    let totalCount = await UserModel.find(finalQuery).count();
-    let users = await UserModel.find(finalQuery).limit(limit);
-
-    if (users.length == 0) {
-      res.send("No users with this details!!!");
-    } else {
-      res.json({
-        message: "success fetching",
-        data: users,
-        totalCount,
-      });
-    }
+    pool.query(sql, function (error, result) {
+      if (error) {
+        console.log(error);
+        res.send(error);
+      } else {
+        if (!result.length) {
+          res.json({ message: "No students with this details!!!", result });
+        } else {
+          // console.log('search result......', result);
+          res.json({ message: "search result of students", result });
+        }
+      }
+    });
   } catch (error) {
     console.log("errorrrrrrr", error);
     res.status(500).send(error);
   }
 };
 
-// function of searching users...................
-function createAdvanceQuery(reqData: any) {
-  return new Promise((res, rej) => {
-    let appendQuery = "";
-
-    let isEmpty = Object.values(reqData).every((x) => x === null || x === "");
-    let objKeys = Object.keys(reqData);
-
-    if (isEmpty) {
-      res("");
-      return;
-    }
-
-    for (let i = 0; i < objKeys.length; i++) {
-      //  console.log('obbbbbbbbbb', objKeys[i]);
-      //  console.log('occcccccccc', reqData[objKeys[i]]);
-
-      if (reqData[objKeys[i]] != null && reqData[objKeys[i]] != undefined) {
-        if (objKeys[i] == "classNumber" && reqData[objKeys[i]] != "") {
-          // console.log('qqqqqqqqq', reqData[objKeys[i]]);
-
-          appendQuery += `"classNumber": ${reqData[objKeys[i]]}  ,`;
-          // console.log('insideeeeee entereddddddd');
-        } else if (objKeys[i] == "searchText" && reqData[objKeys[i]] != "") {
-          // console.log('else if entered...');
-
-          appendQuery += `"$or": [
-            
-            {"name": { "$regex" : "${reqData[objKeys[i]]}", "$options": "i" }},
-            {"email":{ "$regex" : "${reqData[objKeys[i]]}", "$options": "i" }},
-            {"phone":{ "$regex" : "${reqData[objKeys[i]]}", "$options": "i" }}
-          
-              ] ,`;
-        }
-      }
-    }
-
-    let n = appendQuery.lastIndexOf(",");
-    let finalFilterQuery = appendQuery.slice(0, n);
-    // console.log(JSON.stringify(finalFilterQuery));
-    res({ finalFilterQuery: finalFilterQuery });
-  });
-}
-
+// download excel file of users................................
 const exportUsers = async (req: Request, res: Response) => {
   let fromDate: any = req.query.dobFrom;
   let lastDate: any = req.query.dobTo;
@@ -292,7 +328,7 @@ const exportUsers = async (req: Request, res: Response) => {
   // console.log("path addeddd...........", path);
 
   worksheet.columns = [
-    { header: "S no.", key: "_id", width: 10 },
+    { header: "S no.", key: "id", width: 10 },
     { header: "class no.", key: "classNumber", width: 10 },
     { header: "Name", key: "name", width: 10 },
     { header: "Email Id", key: "email", width: 10 },
@@ -303,77 +339,88 @@ const exportUsers = async (req: Request, res: Response) => {
 
   let counter = 1;
 
-  const User = await UserModel.find({
-    dob: {
-      $gte: fromDate,
-      $lt: lastDate,
-    },
-  });
+  const sql = `SELECT * FROM students WHERE dob >= '${fromDate}' AND dob <= '${lastDate}'`;
 
-  // console.log("user coming...........", User);
+  pool.query(sql, function (error, result) {
+    if (error) {
+      console.log(error);
+      // res.send(error)
+    } else {
+      // console.log("rrrrrrrrrrrrrr", result);
 
-  User.forEach((user) => {
-    user.classNumber = counter;
-    worksheet.addRow(user);
-    counter++;
-  });
-
-  worksheet.getRow(1).eachCell((cell: any) => {
-    cell.font = { bold: true };
-  });
-
-  try {
-    const data = await workbook.xlsx.writeFile(v4() + ".xlsx");
-
-    const buffer = await workbook.xlsx.writeBuffer(
-      "excelSheet of student " + fromDate + "-" + lastDate + ".xlsx"
-    );
-
-    // console.log('bbbbbbbbb', buffer);
-
-    let mailTransporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "jayakrishnan@scriptlanes.com",
-        pass: "sivzgeycbgqpmsnz",
-      },
-    });
-
-    let details = {
-      from: "jayakrishnan@scriptlanes.com",
-      to: "jayakrishnansfc43@gmail.com",
-      subject: "Student details in Excel file",
-      // text: "Testing out first sender"
-      html: "content of the users",
-      attachments: [
-        {
-          buffer,
-          content: buffer,
-          contentType:
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        },
-      ],
-    };
-
-    await mailTransporter.sendMail(details, (err: any) => {
-      if (err) {
-        console.log("There is an error ...", err);
-      } else {
-        console.log("email has sent!");
+      if (Array.isArray(result)) {
+        result.forEach((student) => {
+          student.classNumber = counter;
+          worksheet.addRow(student);
+          counter++;
+        });
       }
-    });
 
-    res.send({
-      status: "success",
-      message: "file sent to mail successfully!",
-      // path: `${path}/users.xlsx`,
-    });
-  } catch (error) {
-    res.send({
-      status: "error",
-      message: "Something went wrong",
-    });
-  }
+      worksheet.getRow(1).eachCell((cell: any) => {
+        cell.font = { bold: true };
+      });
+    }
+
+    try {
+      // console.log('tryyyyyyyyyyyyy reached');
+
+      async function excelFunction() {
+        const data = await workbook.xlsx.writeFile(v4() + ".xlsx");
+
+        const buffer = await workbook.xlsx.writeBuffer(
+          "excelSheet of student " + fromDate + "-" + lastDate + ".xlsx"
+        );
+
+        // console.log('bbbbbbbbb', buffer);
+
+        let mailTransporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "jayakrishnan@scriptlanes.com",
+            pass: "sivzgeycbgqpmsnz",
+          },
+        });
+
+        let details = {
+          from: "jayakrishnan@scriptlanes.com",
+          to: "jayakrishnansfc43@gmail.com",
+          subject: "Student details in Excel file",
+          // text: "Testing out first sender"
+          html: "Students details",
+          attachments: [
+            {
+              buffer,
+              content: buffer,
+              contentType:
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            },
+          ],
+        };
+
+        await mailTransporter.sendMail(details, (err: any) => {
+          if (err) {
+            console.log("There is an error ...", err);
+          } else {
+            console.log("email has sent!");
+          }
+        });
+
+        res.send({
+          status: "success",
+          message: "file sent to mail successfully!",
+          // path: `${path}/users.xlsx`,
+        });
+      }
+
+      excelFunction();
+
+    } catch (error) {
+      res.send({
+        status: "error",
+        message: "Something went wrong",
+      });
+    }
+  });
 };
 
 // cron jobs fn................................
@@ -387,6 +434,21 @@ const exportUsers = async (req: Request, res: Response) => {
 //   );
 // });
 
+
+
+// Uploading files with multer    ................................
+const uploadFiles = async (req: Request, res: Response) => {
+
+  try {
+    
+
+
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
 export {
   allUsers,
   createPerson,
@@ -394,4 +456,5 @@ export {
   deletePerson,
   searchUsers,
   exportUsers,
+  uploadFiles
 };
--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -9,19 +9,24 @@ import {
   exportUsers,
   allUsers,
   updateUser,
+  uploadFiles,
 } from "../controllers/users";
 
 const router = express.Router();
 
 router.post("/register", createPerson);
 
+
 router.put("/updateUser/:id", updateUser);
+// router.put("/updateUser/:id", updateUser);
 
 router.delete("/delete/:id", deletePerson);
 
 router.post("/search", searchUsers);
 
 router.get("/downloadExcel", exportUsers);
+
+router.post("/upload_files", uploadFiles);
 
 router.get("/", allUsers);
 
